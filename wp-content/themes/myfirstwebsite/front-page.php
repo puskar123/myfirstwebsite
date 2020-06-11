77ca890f40e6e00f4b8f36ba827133ecd1f6ab26
puskar123 <bhatta141puskar@gmail.com>
2020-06-11 14:33:14 +0545

    banner text changed.

--- wp-content/themes/myfirstwebsite/front-page.php
+++ wp-content/themes/myfirstwebsite/front-page.php
@@ -2,8 +2,8 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
   <div class="page-banner__content container t-center c-white">
-    <h1 class="headline headline--large">Welcome!</h1>
-    <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+    <h1 class="headline headline--large">Hello</h1>
+    <h2 class="headline headline--medium">Bhai&rsquo;bahino.</h2>
     <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
     <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
   </div>
@@ -35,7 +35,8 @@
       get_template_part('template-parts/content', 'event');
       }
       ?>
-      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>"
+       class="btn btn--blue">View All Events</a></p>
 
     </div>
   </div>
