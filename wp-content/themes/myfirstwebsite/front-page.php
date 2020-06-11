8e65e660029f01ff0834fbedca45b428b7fb2f91
puskar123 <bhatta141puskar@gmail.com>
2020-06-11 15:19:21 +0545

    banner text change

--- wp-content/themes/myfirstwebsite/front-page.php
+++ wp-content/themes/myfirstwebsite/front-page.php
@@ -2,8 +2,8 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
   <div class="page-banner__content container t-center c-white">
-    <h1 class="headline headline--large">Hello</h1>
-    <h2 class="headline headline--medium">Bhai&rsquo;bahino.</h2>
+    <h1 class="headline headline--large">Welcome to nepal</h1>
+    <h2 class="headline headline--medium">Please visit our niversity.</h2>
     <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
     <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
   </div>
