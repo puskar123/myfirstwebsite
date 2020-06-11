30bb6a02119a509f8b43e71497ab76801ad951db
puskar123 <bhatta141puskar@gmail.com>
2020-06-11 15:22:29 +0545

    banner text change

--- wp-content/themes/myfirstwebsite/front-page.php
+++ wp-content/themes/myfirstwebsite/front-page.php
@@ -2,9 +2,9 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
   <div class="page-banner__content container t-center c-white">
-    <h1 class="headline headline--large">Welcome to nepal</h1>
-    <h2 class="headline headline--medium">Please visit our niversity.</h2>
-    <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+    <h1 class="headline headline--large">Welcome!</h1>
+    <h2 class="headline headline--medium">Please visit Bhatta Uiversity.</h2>
+    <h3 class="headline headline--small">This is the one of the top university in the world.</h3>
     <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
   </div>
 </div>
